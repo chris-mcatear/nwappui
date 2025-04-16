102854448434d6e6b1bd2b1fe0061b180b68cb24
chris-mcatear <chris.mcatear@hotmail.com>
2025-04-16 10:47:46 +0100

    adsd

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello")
+##UI For Application
+
+import tkinter as tk
+
+#Creating window
+window = tk.Tk()
+window.title("Application Title")
+window.geometry("1280x720")
+
+#Widgets
+scanbutton = tk.Button(window, text="Scan")
+
+window.mainloop()
